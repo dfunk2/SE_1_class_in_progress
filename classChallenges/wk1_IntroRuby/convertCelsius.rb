6ab8762b06644bdbfe3c1e9bce5f8f3d35a10745
Denise <deltafoxtrot27@outlook.com>
2025-10-01 11:58:29 -0700

    weekly class challenge

--- classChallenges/wk1_IntroRuby/convertCelsius.rb
+++ classChallenges/wk1_IntroRuby/convertCelsius.rb
@@ -1,6 +1,7 @@
 def convertCelsius(far)
   celsius = (5 *(far-32))/ 9
-  puts ("#{far} F in Celsius is #{celsius} C") end
+  puts ("#{far} F in Celsius is #{celsius} C") 
+  end
 
 convertCelsius(212)
 convertCelsius(98.6)
